34e7b7551e165c2d9b038856217b42c17d541607
Philipp Gardlowski <84767927+pgardlowski@users.noreply.github.com>
2021-07-02 22:34:11 +0200

    Code wiederhergestellt und Widerstände gefixt

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # timer class
 class Timer:
   def __init__(self):
@@ -71,7 +70,6 @@ def read_men():
   elif ad > 1400 and ad < 1520:
     return 2
   # Menu
-
   #elif ad > 4050:
   elif ad > 1790 and ad < 1880:
     return 3
@@ -88,7 +86,109 @@ if __name__ == '__main__':
   start_rast, alarm = False, False
 
   while True:
-    oled.fill(0)
-    oled.text(str(men.read()), 0, 0)
-    time.sleep_ms(500)
-    oled.show()
+    while l_men == False:
+      ttime, ttemp = 30, 60
+      l_ttime, l_ttemp, l_brew = False, False, False
+      oled.fill(0)
+      oled.text('BRAUSCHLAMPE 1.0', 0, 0)
+      kettle.read_temp_sensor()
+      oled.text('Kessel: {0:5.2f}C'.format(kettle.temp), 0, 20)
+      oled.text('Soll: N/A', 0, 30)
+      oled.text('Heizung: AUS', 0, 40)
+      oled.show()
+      if read_men() == 3:
+        l_men = True
+        time.sleep_ms(500)
+  
+    while l_ttime == False:
+      oled.fill(0)
+      oled.text('BRAUSCHLAMPE 1.0', 0, 0)
+      oled.text('Set time:', 0, 20)
+      oled.text('minutes', 25, 30)
+      if read_men() == 1:
+        ttime += 1
+        time.sleep_ms(50)
+      elif read_men() == 2:
+        if ttime > 0:
+          ttime -= 1
+          time.sleep_ms(50)
+      elif read_men() == 3:
+        l_ttime = True
+        l_men, l_ttemp, l_brew = False, False, False
+        time.sleep_ms(500)
+      elif read_men() == 4:
+        l_ttime = True
+        time.sleep_ms(500)
+      oled.text(str(ttime),0, 30)
+      oled.show()
+  
+    while l_ttemp == False:
+      oled.fill(0)
+      oled.text('BRAUSCHLAMPE 1.0', 0, 0)
+      oled.text('Set time:', 0, 20)
+      oled.text(str(ttime), 0, 30)
+      oled.text('minutes', 20, 30)
+      oled.text('Set temp:', 0, 40)
+      oled.text('Celsius', 30, 50)
+  
+      if read_men() == 1:
+        if ttemp < 105:
+          ttemp += 1
+          time.sleep_ms(50)
+      elif read_men() == 2:
+        if ttemp > 30:
+          ttemp -= 1
+          time.sleep_ms(50)
+      elif read_men() == 3:
+        l_ttemp = True
+        l_men = False
+        time.sleep_ms(500)
+      elif read_men() == 4:
+        l_ttemp = True
+        time.sleep_ms(500)
+        l_men = False
+        kettle.set_target_temp(ttemp)
+        ttime = ttime*60
+      oled.text(str(ttemp),0, 50)
+      oled.show()
+
+    while l_brew == False:
+      oled.fill(0)
+      oled.text('BRAUSCHLAMPE 1.0', 0, 0)
+      #kettle.read_temp_sensor()
+      kettle.update_heating()
+      oled.text('Kessel: {0:5.2f}C'.format(kettle.temp), 0, 20)
+      oled.text('Soll: {0:5.2f}'.format(ttemp), 0, 30)
+      if kettle.heating_state == 1:
+        rel1.value(1)
+        rel2.value(0)
+        oled.text('Heizung: 1/2 AN', 0, 40)
+      elif kettle.heating_state == 2:
+        rel1.value(1)
+        rel2.value(1)
+        oled.text('Heizung: 2/2 AN', 0, 40)
+      else:
+        rel1.value(0)
+        rel2.value(0)
+        oled.text('Heizung: AUS', 0, 40)
+        if start_rast == False:
+          timer.start()
+          start_rast = True
+      tms = divmod(ttime-timer.delta_t,60)
+      oled.text('Time: {0}:{1}'.format(zfill(str(tms[0]),2),zfill(str(tms[1]),2)), 0, 50)
+      oled.show()
+      if read_men() == 3:
+        l_brew,l_men = True,False
+        time.sleep_ms(500)
+
+      if ttime-timer.delta_t < 0:
+        l_brew, alarm = True, True
+        while alarm == True:
+          buzz.value(1)
+          time.sleep_ms(1)
+          buzz.value(0)
+          time.sleep_ms(1)
+          if read_men() == 3:
+            alarm == False
+      if start_rast == True:
+        timer.update()
